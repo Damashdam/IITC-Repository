0ccfb0cddf97f481dfb4cc93c95fbbd39824c312
Damashdam <121407251+Damashdam@users.noreply.github.com>
2024-04-11 09:09:19 +0300

    Add files via upload

---
+++ home work exc2/index.js
@@ -0,0 +1,131 @@
+// let text1 = document.querySelector(".innerhtml")
+
+
+/////mission 1//////
+
+
+// let array = "";
+// for (var i = 1; i <= 10; i++) {
+//     array = ""
+//     for (var j = 1; j <= 10; j++) {
+//         array += j * i;
+//     };
+//     console.log(array);
+
+// };
+
+/////mission 2//////
+
+
+// function higher_or_lower(first, second) {
+//     if (first > second) {
+//         return "first number is higher";
+//     } else if (first < second) {
+//         return "second num is lower";
+//     };
+
+
+
+/////mission 3//////
+
+
+// let user_input1 = parseInt(prompt("enter first number"))
+// let user_input2 = parseInt(prompt("enter second number"))
+
+// let sum = user_input1 + user_input2;
+
+// console.log(sum);
+
+// text1.innerHTML = sum;
+
+
+/////mission 4//////
+
+
+
+// function add_previous(number) {
+//     for (var i = 0; i <= number; i++) {
+//         for (var j = 0; j <= number; j++) {
+//             index = number + j;
+//         }
+//     }
+//     console.log(index);
+// };
+
+// add_previous(5);
+
+
+/////mission 5//////
+
+
+
+// function switch_val(var1, var2) {
+//     console.log("var1=", var1)
+//     console.log("var2=", var2)
+
+//     var temp = var1;
+//     var1 = var2;
+//     var2 = temp;
+
+//     console.log("after switch");
+//     console.log("var1=", var1);
+//     console.log("var2=", var2);
+// }
+
+// switch_val(3, 4);
+
+/////mission 6//////
+
+
+
+// function rectangle(num) {
+//     for (let i = 0; i < num; i++) {
+//         let row = "";
+//         for (let j = 0; j < num - i; j++) {
+//             row += "*";
+//         }
+//         console.log(row);
+//     }
+
+// };
+// rectangle(5);
+
+
+//////mission 7////
+
+
+// const array = [5, 3, 7, 8, 1];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//     console.log(list);
+// };
+
+// const average = sum / numbers.length;
+
+// console.log("Sum of elements:", sum);
+// console.log("Average:", average);
+
+//////mission 8////
+
+
+// const names = ["daniel", "eden", "dan", "gaya", "natalia"]
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+// ////mission 9////
+
+
+// let array = [];
+// for (let i = 0; i <= 100; i++) {
+//     array.push(i);
+//     for (let j = 0; j < i; j++) {
+//         console.log(array);
+//     }
+// }
+
+
+///mission 10////
